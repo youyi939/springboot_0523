e4fc4c1fad0ea23b730e10f14086e8f85b587054
youyi <1291174818@qq.com>
2020-05-23 12:10:16 +0800

    cghhgc

--- ci.sh
+++ ci.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 git pull
 mvn clean package
-#docker-compose down
-#docker-compose up -d
-
-java -jar target/springboot_0523-0.0.1-SNAPSHOT.jar
+docker-compose down
+docker-compose up -d
